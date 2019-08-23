888ac660928656e6604896548a0646e49ed20a22
wa1tnr <wa1tnr@users.noreply.github.com>
2019-08-23 15:16:13 +0000

    GPIO handling - in comments

--- ring-led-aa/ring-led-aa-sketch.cpp
+++ ring-led-aa/ring-led-aa-sketch.cpp
@@ -34,15 +34,36 @@ int pop() {
 
 // make TOS pin an output
 void output() {
+// 111   push(TOS);
   pinMode(pop(), OUTPUT);
 }
 
 
+
+void led_off(void) {
+// 226   int a = pop();
+// 232   digitalWrite(a, LOW);
+}
+
+/*
+223 // Toggle pin at TOS and delay(spd), repeat...
+224 NAMED(_wiggle, "wiggle");
+225 void wiggle() {
+226   int a = pop();
+227   pinMode(a, OUTPUT);
+228   for (int i = 0; i < 20; i++) {
+229     digitalWrite(a, HIGH);
+230     defspeed();
+231     delay(spd); // delay(100);
+232     digitalWrite(a, LOW);
+233     delay(spd); // delay(100);
+234   }
+235 }
+*/
+
 void setup_gpio(void) {
   output();
 }
-
-void led_off(void) {}
 
 void setup(void) {
   setup_gpio();
